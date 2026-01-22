ac553f4c83ffab7e209d20d52995ea7395004d65
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2026-01-22 16:22:36 +0500

    sidebar icon color updated

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -40,34 +40,24 @@ const handleNavClick = () => {
 </script>
 
 <template>
-  <aside
-    :class="[
-      'sidebar w-64 bg-white p-6 md:p-8 flex flex-col justify-between',
-      isSidebarOpen ? 'sidebar--open' : '',
-    ]"
-  >
-    <button
-      @click="closeSidebar"
-      class="sidebar__close-btn md:hidden absolute top-7 right-6 w-10 h-10 flex items-center justify-center rounded-full border border-[#F5F5F7] hover:bg-gray-50 transition-all cursor-pointer z-10"
-    >
+  <aside :class="[
+    'sidebar w-64 bg-white p-6 md:p-8 flex flex-col justify-between',
+    isSidebarOpen ? 'sidebar--open' : '',
+  ]">
+    <button @click="closeSidebar"
+      class="sidebar__close-btn md:hidden absolute top-7 right-6 w-10 h-10 flex items-center justify-center rounded-full border border-[#F5F5F7] hover:bg-gray-50 transition-all cursor-pointer z-10">
       <X :size="20" class="text-[#8E92BC]" />
     </button>
 
     <div class="flex flex-col">
       <img src="@/assets/logo.svg" alt="Logo" class="w-auto h-10" />
-      <nav class="space-y-6 mt-15">
-        <router-link
-          v-for="item in navItems"
-          :key="item.label"
-          :to="item.path"
-          @click="handleNavClick"
-          :class="[
-            'w-full flex items-center gap-3 px-5 py-2.5 rounded-[10px] transition-all text-left text-sm cursor-pointer',
-            isActive(item.path)
-              ? 'bg-gray-100 text-black font-semibold'
-              : 'text-gray-600 hover:bg-gray-50',
-          ]"
-        >
+      <nav class="space-y-4 mt-15">
+        <router-link v-for="item in navItems" :key="item.label" :to="item.path" @click="handleNavClick" :class="[
+          'w-full flex items-center gap-3 px-5 py-2.5 rounded-[10px] transition-all text-left text-sm cursor-pointer',
+          isActive(item.path)
+            ? 'bg-gray-100 text-black font-semibold'
+            : 'text-gray-600 hover:bg-gray-50',
+        ]">
           <component :is="item.icon" />
           <span class="text-sm leading-[150%] tracking-[-2%]">
             {{ item.label }}
